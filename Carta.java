32c3779e32a559ef87a25376d88a6b2b13157a16
Kennet2003 <kemeroca@espol.edu.ec>
2024-07-07 10:48:04 -0700

    Add files via upload

---
+++ Carta.java
@@ -0,0 +1,15 @@
+package ec.edu.espol.clases;
+
+public abstract class Carta {
+
+    protected char color;
+
+    public Carta(char color) {
+        this.color = color;
+    }
+
+    public char getColor() {
+        return color;
+    }
+    
+}
